163b54234085081355ae1d7d1426722f3780b020
laurentbarraud <lau.barraud@gmail.com>
2025-09-11 00:31:45 +0200

    Adaptation finale du z-order des watermarks. Tous les watermarks s'affichent correctement.

--- chat-client/MVVM/View/MainWindow.xaml.cs
+++ chat-client/MVVM/View/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿/// <file>MainWindow.cs</file>
 /// <author>Laurent Barraud</author>
 /// <version>1.0</version>
-/// <date>September 10th, 2025</date>
+/// <date>September 11th, 2025</date>
 
 using chat_client.Helpers;
 using chat_client.MVVM.View;
